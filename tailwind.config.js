cf593768b6b961009797b6718c561636fe8f6910
ethantech-2925 <clonewplace@gmail.com>
2025-11-22 23:37:20 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // Thêm file bạn dùng Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
